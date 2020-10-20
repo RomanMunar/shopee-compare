f478e56de691b8b8d08fb5a85ae505776a4d6de7
RomanMunar <roman.munar1@gmail.com>
2020-10-20 15:31:08 +0800

    added shadow utility

--- frontend/src/styles.ts
+++ frontend/src/styles.ts
@@ -25,3 +25,36 @@ export const color = {
   borderLight: "#C1C7D0",
   borderInputFocus: "#4c9aff",
 };
+
+export const adjustColor = {
+  darken: (colorValue: string, amount: number) =>
+    Color(colorValue).darken(amount).string(),
+  lighten: (colorValue: string, amount: number) =>
+    Color(colorValue).lighten(amount).string(),
+  rgba: (colorValue: string, opacity: number) =>
+    Color(colorValue).alpha(opacity).string(),
+};
+
+export const shadows = {
+  shadowSm: css`
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  `,
+  shadow: css`
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  `,
+  shadowMd: css`
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  `,
+  shadowLg: css`
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+      0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  `,
+  shadowXl: css`
+    box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+      0 10px 10px -5px rgba(0, 0, 0, 0.04);
+  `,
+  shadow2Xl: css`
+    box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
+  `,
+};

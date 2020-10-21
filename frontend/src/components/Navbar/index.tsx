30b300ed571579b8a0571f557129ff5480a1202a
RomanMunar <roman.munar1@gmail.com>
2020-10-21 23:13:46 +0800

    Navbar Component

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { ReactComponent as AboutIcon } from "../../assets/help.svg";
+import {
+  NavLeft,
+  LogoLink,
+  StyledLogo,
+  Bottom,
+  Item,
+  ItemText,
+} from "./Styles";
 
 interface Props {
   text?: string;
 }
 
-export const Navbar = (props: Props) => <div>{props.text}</div>;
+export const Navbar = (props: Props) => (
+  <NavLeft>
+    <LogoLink to='/'>
+      <StyledLogo />
+    </LogoLink>
+
+    <Item>
+      {/* <Icon type='search' size={22} top={1} left={3} /> */}
+      <ItemText>Search issues</ItemText>
+    </Item>
+
+    <Item>
+      {/* <Icon type='plus' size={27} /> */}
+      <ItemText>Create Issue</ItemText>
+    </Item>
+
+    <Bottom>
+      <AboutIcon />
+    </Bottom>
+  </NavLeft>
+);

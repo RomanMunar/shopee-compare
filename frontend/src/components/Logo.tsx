bb744707226c4d7dc71f6e1759a670d855cf19e5
RomanMunar <roman.munar1@gmail.com>
2020-10-24 19:22:42 +0800

    formated attributes to work well with react

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -14,21 +14,21 @@ export const Logo = () => (
         d='M4 12H44V20L42.6015 20.8391C40.3847 22.1692 37.6153 22.1692 35.3985 20.8391L34 20L32.6015 20.8391C30.3847 22.1692 27.6153 22.1692 25.3985 20.8391L24 20L22.6015 20.8391C20.3847 22.1692 17.6153 22.1692 15.3985 20.8391L14 20L12.6015 20.8391C10.3847 22.1692 7.61531 22.1692 5.39853 20.8391L4 20V12Z'
         fill='#2F88FF'
         stroke='#333'
-        stroke-width='4'
+        strokeWidth='4'
         stroke-linecap='butt'
         stroke-linejoin='round'
       />
       <path
         d='M8 22.4889V44H40V22'
         stroke='#333'
-        stroke-width='4'
+        strokeWidth='4'
         stroke-linecap='butt'
         stroke-linejoin='round'
       />
       <path
         d='M8 11.8222V4H40V12'
         stroke='#333'
-        stroke-width='4'
+        strokeWidth='4'
         stroke-linecap='butt'
         stroke-linejoin='round'
       />
@@ -39,7 +39,7 @@ export const Logo = () => (
         height='12'
         fill='#2F88FF'
         stroke='#333'
-        stroke-width='4'
+        strokeWidth='4'
         stroke-linecap='butt'
         stroke-linejoin='round'
       />

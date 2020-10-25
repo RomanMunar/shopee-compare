7889192bdceba2c8069fb502d88458f6bf0f75fb
RomanMunar <roman.munar1@gmail.com>
2020-10-25 21:26:33 +0800

    removed select, styled button and dropdown instead

--- frontend/src/Search/Searchbar/index.tsx
+++ frontend/src/Search/Searchbar/index.tsx
@@ -1,55 +1,52 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useRef, useState } from "react";
 import styled from "styled-components";
 import { Icon } from "../../components/Icon";
+import Select from "../../components/Select";
 import { font } from "../../styles";
+import useOnOutsideClick from "../../useOnOutsideClick";
 import { StyledInput, InputElement } from "./Styles";
 
-export default (): ReactElement => (
-  <StyledInput>
-    <div
-      style={{
-        position: "absolute",
-        pointerEvents: "none",
-        top: "4px",
-        left: "8px",
-      }}
-    >
-      <Icon size={24} type='SearchBar' />
-    </div>
-    <InputElement />
-    <Filter>
-      <Select>
-        <option dir='rtl' selected disabled hidden>
-          Sort By
-        </option>
+export default (): ReactElement => {
+  const [PriceDropdownOpen, setPriceDropdownOpen] = useState(false);
+  const [testDropdownOpen, setTestDropdownOpen] = useState(false);
+  const $priceRef = useRef<HTMLDivElement>(null);
+  const [SortDropdownOpen, setSortDropdownOpen] = useState(false);
+  const $sortRef = useRef<HTMLDivElement>(null);
+  useOnOutsideClick($sortRef, () => setSortDropdownOpen(!SortDropdownOpen));
+  useOnOutsideClick($priceRef, () => setPriceDropdownOpen(!PriceDropdownOpen));
+  return (
+    <StyledInput>
+      <div
+        style={{
+          position: "absolute",
+          pointerEvents: "none",
+          top: "5px",
+          left: "9px",
+        }}
+      >
+        <Icon size={24} type='SearchBar' />
+      </div>
+      <InputElement />
+      <Filter>
+        <Select
+          DropdownOpen={testDropdownOpen}
+          setDropdownOpen={setTestDropdownOpen}
+          title='Price'
+          options={["₱0-200", "₱200-500", "₱500-1000", "₱1000-2000"]}
+        />
+        <Select
+          DropdownOpen={SortDropdownOpen}
+          setDropdownOpen={setSortDropdownOpen}
+          title='Sort By'
+          options={["Relevance", "Sales", "Price", "Rating"]}
+        />
+      </Filter>
+    </StyledInput>
+  );
+};
 
-        {["Relevance", "Sales", "Recent", "Rating"].map((option) => (
-          <option dir='rtl'>{option}</option>
-        ))}
-      </Select>
-      <Select>
-        <option dir='rtl' selected disabled hidden>
-          Price Range
-        </option>
-
-        {["₱0-200", "₱200-500", "₱500-1000", "₱1000-2000"].map((option) => (
-          <option dir='rtl'>{option}</option>
-        ))}
-      </Select>
-    </Filter>
-  </StyledInput>
-);
-
-const Select = styled.select`
-  border: 0px;
-  background-color: transparent;
-  ${font.medium}
-  color: #172b4d;
-  margin-left: -5px;
-  text-align-last: right;
-`;
 const Filter = styled.div`
-  top: 4px;
+  top: 2px;
   right: 4px;
   ${font.medium}
   position: absolute;

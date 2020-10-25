d6dbab534aedd3eb6059b972678b9da5ef4d0a5b
RomanMunar <roman.munar1@gmail.com>
2020-10-25 21:26:48 +0800

    added route container component

---
+++ frontend/src/components/Container.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement, ReactNode } from "react";
+import styled from "styled-components";
+
+interface Props {
+  children: ReactNode;
+}
+
+export default ({ children }: Props): ReactElement => {
+  return <Container>{children}</Container>;
+};
+
+export const Container = styled.main`
+  margin-left: 80px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;

2cc604dc6e2f0f78db10353ea4115b924173d58a
RomanMunar <roman.munar1@gmail.com>
2020-10-27 16:55:55 +0800

    added filterByField utility

--- frontend/src/shared/utils/utils.ts
+++ frontend/src/shared/utils/utils.ts
@@ -1,3 +1,5 @@
+import { SearchItem } from "../../interfaces";
+
 export const kFormatter = (num: number) =>
   Math.abs(num) > 999 ? (Math.abs(num) / 1000).toFixed(1) + "k" : Math.abs(num);
 
@@ -21,3 +23,22 @@ export const priceCompare = ({
             .slice(0, price_max.toString().split("").length - 5)
         )
       )}`;
+
+export const filterByField = (array: any[], field: string) => {
+  const displayedResults: SearchItem[] = [];
+  array.filter(function (item) {
+    return displayedResults.findIndex((x: any) => x[field] === item[field]) ===
+      -1
+      ? displayedResults.push(item)
+      : null;
+  });
+  return displayedResults;
+};
+
+export const reorder = (list: SearchItem[], startIndex: number, endIndex: number) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};

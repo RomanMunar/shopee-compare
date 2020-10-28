1a45af28dc34da2709d269ade802e3e685eadd3d
RomanMunar <roman.munar1@gmail.com>
2020-10-29 00:19:50 +0800

    added button component

---
+++ frontend/src/components/Button/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { color } from "../../styles";
+
+import { StyledButton, StyledSpinner, Text } from "./Styles";
+
+const Button = ({
+  children,
+  variant,
+  disabled,
+  isWorking,
+  onClick,
+  ...buttonProps
+}) => {
+  const handleClick = () => {
+    if (!disabled && !isWorking) {
+      onClick();
+    }
+  };
+
+  return (
+    <StyledButton
+      {...buttonProps}
+      onClick={handleClick}
+      variant={variant}
+      disabled={disabled || isWorking}
+      iconOnly={!children}
+    >
+      {isWorking && <StyledSpinner size={26} color={getIconColor(variant)} />}
+      {children && <Text withPadding={isWorking}>{children}</Text>}
+    </StyledButton>
+  );
+};
+
+const getIconColor = (variant) =>
+  ["secondary", "empty"].includes(variant) ? color.textDark : "#fff";
+
+export default Button;

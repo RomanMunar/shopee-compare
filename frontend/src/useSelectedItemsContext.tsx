6d30b836bd9f9c761ec31f253872550914192de3
RomanMunar <roman.munar1@gmail.com>
2020-10-28 13:33:48 +0800

    context hook for selected items

---
+++ frontend/src/useSelectedItemsContext.tsx
@@ -0,0 +1,25 @@
+import React, { createContext, ReactNode, useState } from "react";
+import { SearchItem } from "./interfaces";
+
+const initialState: {
+  selectedItems: SearchItem[];
+  setSelectedItems: React.Dispatch<React.SetStateAction<SearchItem[]>>;
+} = {
+  selectedItems: [],
+  setSelectedItems: () => [{}],
+};
+
+export const SelectedItemsContext = createContext(initialState);
+export default function SelectedItemsProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [selectedItems, setSelectedItems] = useState<SearchItem[]>([]);
+
+  return (
+    <SelectedItemsContext.Provider value={{ selectedItems, setSelectedItems }}>
+      {children}
+    </SelectedItemsContext.Provider>
+  );
+}

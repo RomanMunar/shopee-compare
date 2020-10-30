cc621c1f3dff49917bfb8cd7b4fec72c73f1ff9a
RomanMunar <roman.munar1@gmail.com>
2020-10-30 18:56:03 +0800

    removed 100% width

--- frontend/src/components/Tag/index.tsx
+++ frontend/src/components/Tag/index.tsx
@@ -11,13 +11,15 @@ export default ({ primary, children }: Props): ReactElement => {
   return <Tag>{children}</Tag>;
 };
 
-const Tag = styled.span<Props>`
+export const Tag = styled.span<Props>`
+  display:inline-flex;
+  justify-content:center;
+  align-items:center;
   padding: 0.5rem 1rem;
   cursor: point;
   word-wrap: none;
   white-space: nowrap;
   border-radius: 9999px;
-  width: 100%;
   padding: 5px 10px;
   margin-right: 0.35rem;
   background-color: ${(props) =>

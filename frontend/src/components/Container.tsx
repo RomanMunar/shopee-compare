b4eddefa2f229ad80c561e9013aad47aa15397c4
RomanMunar <roman.munar1@gmail.com>
2020-11-04 21:03:12 +0800

    changed from margin to padding.

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -11,8 +11,10 @@ export default ({ children }: Props): ReactElement => {
 
 export const Container = styled.main`
   overflow: hidden;
-  margin-left: 80px;
+  padding-left: 80px;
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  background-color: #fff;
 `;

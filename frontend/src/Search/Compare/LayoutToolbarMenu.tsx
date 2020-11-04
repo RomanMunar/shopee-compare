328931a75f3b1f905916a42f6752108094effcb0
RomanMunar <roman.munar1@gmail.com>
2020-11-04 21:06:36 +0800

    removed unused wrapper component

--- frontend/src/Search/Compare/LayoutToolbarMenu.tsx
+++ frontend/src/Search/Compare/LayoutToolbarMenu.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ToolbarButton } from "../../components/Toolbar";
 import { Toolbar } from "../../components/Toolbar/Styles";
 import { Layout } from "../../interfaces";
-import { MenuWrapper } from "./Styles";
 
 interface Props {
   setLayout: React.Dispatch<React.SetStateAction<Layout>>;
@@ -10,28 +9,26 @@ interface Props {
 
 const LayoutToolbarMenu = ({ setLayout }: Props) => {
   return (
-    <MenuWrapper>
-      <Toolbar withoutMargin place='right-top'>
-        <ToolbarButton
-          onClick={() => setLayout("main")}
-          name='Main Layout'
-          icon='MainLayout'
-          tooltipPlace='bottom'
-        />
-        <ToolbarButton
-          onClick={() => setLayout("double")}
-          name='Double Layout'
-          icon='DoubleLayout'
-          tooltipPlace='bottom'
-        />
-        <ToolbarButton
-          onClick={() => setLayout("none")}
-          name='No Layout'
-          icon='Column'
-          tooltipPlace='bottom'
-        />
-      </Toolbar>
-    </MenuWrapper>
+    <Toolbar withoutMargin place='right-top'>
+      <ToolbarButton
+        onClick={() => setLayout("main")}
+        name='Main Layout'
+        icon='MainLayout'
+        tooltipPlace='bottom'
+      />
+      <ToolbarButton
+        onClick={() => setLayout("double")}
+        name='Double Layout'
+        icon='DoubleLayout'
+        tooltipPlace='bottom'
+      />
+      <ToolbarButton
+        onClick={() => setLayout("none")}
+        name='No Layout'
+        icon='Column'
+        tooltipPlace='bottom'
+      />
+    </Toolbar>
   );
 };
 

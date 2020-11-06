f0701331878972ec0660dabb043eb38f11ad1c10
RomanMunar <roman.munar1@gmail.com>
2020-11-06 18:14:42 +0800

    added more flex utilities

--- frontend/src/components/Flex.tsx
+++ frontend/src/components/Flex.tsx
@@ -11,22 +11,23 @@ type Props = {
 
 const FlexStyle = styled.div<{
   gap?: number;
-  overflow?:"hidden"|"scroll";
+  overflow?: "hidden" | "scroll";
   align?: "center" | "flex-start" | "flex-end";
-  justify?: "center" | "flex-start" | "flex-end" | "between" | "around";
+  justify?: "center" | "flex-start" | "flex-end" | "space-between" | "around";
   grow?: number;
   shrink?: number;
   dir?: "column" | "row" | "column-reverse" | "row-reverse";
   wrap?: "wrap" | "nowrap";
 }>`
   display: flex;
-  align-items: ${(props) => (props.align ? props.align : "center")};
-  gap: ${(props) => (props.gap ? `${props.gap}%` : "0")};
-  justify-content: ${(props) => (props.justify ? props.align : "center")};
-  flex-grow: ${(props) => (props.grow ? props.grow : "0")};
-  flex-shrink: ${(props) => (props.shrink ? props.shrink : "0")};
-  flex-direction: ${(props) => (props.dir ? props.dir : "column")};
-  flex-wrap: ${(props) => (props.wrap ? props.wrap : "nowrap")};
+  align-items: ${(props) => props.align && props.align};
+  overflow: ${(props) => props.overflow && props.overflow};
+  gap: ${(props) => props.gap && `${props.gap}%`};
+  justify-content: ${(props) => props.justify && props.justify};
+  flex-grow: ${(props) => props.grow && props.grow};
+  flex-shrink: ${(props) => props.shrink && props.shrink};
+  flex-direction: ${(props) => props.dir && props.dir};
+  flex-wrap: ${(props) => props.wrap && props.wrap};
 `;
 
 export default FlexStyle;

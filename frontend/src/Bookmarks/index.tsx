f2c6ff192a28ca2a1ec8c59270c7f51ed74f3b5e
RomanMunar <roman.munar1@gmail.com>
2020-11-10 19:40:34 +0800

    removed unused files and deps

--- frontend/src/Bookmarks/index.tsx
+++ frontend/src/Bookmarks/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Container from "../components/Container";
-import { SearchItem } from "../interfaces";
-import { mockData } from "../Search/mochResponses";
 
 const Bookmarks = () => {
   return <Container> Bookmarks </Container>;

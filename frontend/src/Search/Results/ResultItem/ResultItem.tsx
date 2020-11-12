95947240206887b18a1b9abf5840c0af4a7bcc90
RomanMunar <roman.munar1@gmail.com>
2020-11-12 23:37:15 +0800

    removed styling bug

--- frontend/src/Search/Results/ResultItem/ResultItem.tsx
+++ frontend/src/Search/Results/ResultItem/ResultItem.tsx
@@ -1,47 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
 import GridStats from "../../../components/GridStats";
+import { ToolbarButton } from "../../../components/Toolbar";
 import { ListItem, SearchItem } from "../../../interfaces";
 import { useUI } from "../../../shared/contexts/useUIContext";
 import { ResultItemImage } from "../ResultItemImage";
+import { AddRowButton } from "../Results.styles";
 import { RItem, ResultItemTitle } from "./ResultItem.stlyes";
 
 interface Props {
   index: number;
-  item: SearchItem;
-  itemid: number;
+  item: ListItem<SearchItem>;
   initialSelectedItems: ListItem<SearchItem>[];
+  setInitialSelectedItems: React.Dispatch<
+    React.SetStateAction<ListItem<SearchItem>[]>
+  >;
 }
 
-const ResultItem = ({ initialSelectedItems, itemid, item, index }: Props) => {
-  const { displayMaxSearchPanel } = useUI();
+const ResultItem = ({
+  initialSelectedItems,
+  item,
+  index,
+  setInitialSelectedItems,
+}: Props) => {
+  const { item: itm } = item;
 
+  const { displayMaxSearchPanel } = useUI();
+  const [showAddRow, setShowAddRow] = useState(false);
+  const [showRemoveRow, setShowRemoveRow] = useState(false);
+  const selected = initialSelectedItems
+    .map((i) => i.itemid)
+    .includes(item.itemid);
   return (
     <Draggable
       isDragDisabled={initialSelectedItems
         .map((i) => i.itemid)
-        .includes(itemid)}
-      key={"compare-item-" + item.itemid}
-      draggableId={`res-${itemid}`}
+        .includes(item.itemid)}
+      key={"compare-item-" + itm.itemid}
+      draggableId={`res-${item.itemid}`}
       index={index}
     >
       {(provided, snapshot) => (
         <RItem
+          onMouseEnter={() => {
+            setShowAddRow(true);
+            setShowRemoveRow(true);
+          }}
+          onMouseLeave={() => {
+            setShowAddRow(false);
+            setShowRemoveRow(false);
+          }}
           big={displayMaxSearchPanel}
-          key={`initialItem-${item.itemid}`}
-          selected={initialSelectedItems.map((i) => i.itemid).includes(itemid)}
+          key={`initialItem-${itm.itemid}`}
+          selected={selected}
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           isDragging={snapshot.isDragging}
         >
-          <ResultItemImage src={item.image} />
+          <ResultItemImage src={itm.image} />
           <div style={{ padding: "2px 8px 5px 8px" }}>
             <ResultItemTitle>
-              {item.name.replace(/[^a-zA-Z0-9 ]/g, "")}
+              {itm.name.replace(/[^a-zA-Z0-9 ]/g, "")}
             </ResultItemTitle>
-            <GridStats item={item} on='results' />
+            <GridStats item={itm} on='results' />
           </div>
+          {!selected ? (
+            <AddRowButton
+              onClick={() => setInitialSelectedItems((prev) => [...prev, item])}
+              right
+              showAddRow={showAddRow}
+            >
+              +
+            </AddRowButton>
+          ) : (
+            <AddRowButton
+              onClick={() =>
+                setInitialSelectedItems((prev) =>
+                  prev.filter((res) => item.itemid !== res.itemid)
+                )
+              }
+              right
+              remove
+              showAddRow={showRemoveRow}
+            >
+              —
+            </AddRowButton>
+          )}
         </RItem>
       )}
     </Draggable>

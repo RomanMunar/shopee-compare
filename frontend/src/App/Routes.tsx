6f309db5009b4d2993a92ef353ce3380184b5085
RomanMunar <roman.munar1@gmail.com>
2020-11-12 23:30:47 +0800

    remounted bookmarks component

--- frontend/src/App/Routes.tsx
+++ frontend/src/App/Routes.tsx
@@ -7,6 +7,7 @@ import { Navbar } from "../components/Navbar";
 import { Search } from "../Search";
 import Settings from "../Settings";
 import { UIProvider } from "../shared/contexts/useUIContext";
+import Bookmarks from "../Bookmarks/Bookmarks";
 
 const Router = () => (
   <BrowserRouter>
@@ -15,7 +16,7 @@ const Router = () => (
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/search/*' element={<Search />} />
-        <Route path='/bookmarks' element={<Main />} />
+        <Route path='/bookmarks' element={<Bookmarks />} />
         <Route path='/settings' element={<Settings />} />
         <Route path='/authenticate' element={<Auth />} />
         <Route path='*' element={<PageError />} />

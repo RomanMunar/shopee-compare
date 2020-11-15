bd3dd789014766ef46975fe59dd72a68491e4501
RomanMunar <roman.munar1@gmail.com>
2020-11-15 08:06:01 +0800

    searchbar changes url params

--- frontend/src/Search/SearchBar/SearchBar.tsx
+++ frontend/src/Search/SearchBar/SearchBar.tsx
@@ -1,35 +1,79 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
+import { Sort } from "../../interfaces";
+import { useParams, useSearchParams } from "react-router-dom";
+import useTimeoutFn from "../../shared/hooks/useTimeout";
+import { useKeyPress } from "../../shared/hooks/useKeyPressed";
 import { Icon } from "../../components/Icon";
-import Select from "../../components/Select";
+import { Select } from "../../components/Select";
 import {
   Filter,
   IconWrapper,
   InputElement,
   StyledInput,
 } from "./SearchBar.styles";
+import { getSettings } from "../../shared/utils/localStorage";
 
 const SearchBar = (): ReactElement => {
-  const [PriceDropdownOpen, setPriceDropdownOpen] = useState(false);
-  const [SortDropdownOpen, setSortDropdownOpen] = useState(false);
+  const {preference} = getSettings();
+  const [params, setParams] = useSearchParams();
+  const search = () => setParams({ ...params, ...paramsSession });
+  const [isReady, cancel, reset] = useTimeoutFn(search, 1000);
+  const [page, setPage] = useState(0);
+  const [order, setOrder] = useState<"desc" | "asc">("desc");
+  const [keywordInput, setKeywordInput] = useState("");
+  const [sortOption, setSortOption] = useState(preference.searchSort);
+  const [priceRangeOption, setPriceRangeOption] = useState("");
+  const [pastKeywordInput, setPastKeywordInput] = useState("");
+  const [paramsSession, setParamsSession] = useState({});
+  const [price_min, price_max] = priceRangeOption
+    .split("-")
+    .map((i) => parseInt(i));
+  useEffect(() => {
+    const newParams: any = {};
+    if (sortOption) newParams.by = sortOption;
+    if (price_min) newParams.price_min = price_min;
+    if (price_max) newParams.price_max = price_max;
+    if (page) newParams.page = page;
+    if (order) newParams.order = order;
+    setParamsSession({
+      locations: preference.sellerLocation.toString(),
+      keyword: keywordInput.trim(),
+      ...newParams,
+    });
+  }, [page, order, keywordInput, sortOption, priceRangeOption]);
+  const escKeyPressed = useKeyPress("Escape");
+  useEffect(() => {
+    if (
+      escKeyPressed ||
+      !keywordInput ||
+      keywordInput === "" ||
+      pastKeywordInput === keywordInput
+    )
+      return cancel();
+    setPastKeywordInput(keywordInput);
+    reset();
+  }, [keywordInput, escKeyPressed]);
 
   return (
     <StyledInput>
       <IconWrapper>
         <Icon size={24} type='SearchBar' />
       </IconWrapper>
-      <InputElement />
+      <InputElement onChange={(e) => setKeywordInput(e.target.value)} />
       <Filter>
         <Select
-          DropdownOpen={PriceDropdownOpen}
-          setDropdownOpen={setPriceDropdownOpen}
+          selectedOption={priceRangeOption}
+          setSelectedOption={setPriceRangeOption}
           title='Price'
-          options={["₱0-200", "₱200-500", "₱500-1000", "₱1000-2000"]}
+          lead='₱'
+          options={["0-200", "200-500", "500-1000", "1000-2000"]}
         />
         <Select
-          DropdownOpen={SortDropdownOpen}
-          setDropdownOpen={setSortDropdownOpen}
+          selectedOption={sortOption}
+          setSelectedOption={setSortOption}
           title='Sort By'
-          options={["Relevance", "Sales", "Price", "Rating"]}
+          lead='By '
+          options={["relevance", "sales", "price", "latest"]}
         />
       </Filter>
     </StyledInput>

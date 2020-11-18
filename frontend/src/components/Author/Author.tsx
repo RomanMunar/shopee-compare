9b9cbb2287fe9909d7b31c9e5944d14958056bd8
RomanMunar <roman.munar1@gmail.com>
2020-11-18 11:00:57 +0800

    author now uses real data

--- frontend/src/components/Author/Author.tsx
+++ frontend/src/components/Author/Author.tsx
@@ -3,35 +3,19 @@ import { Icon } from "../Icon";
 import { AuthorImage, AuthorWrapper } from "./Author.styles";
 
 interface Props {
-  name?: string;
-  src?: string;
-  aveStar?: number;
+  name: string;
+  src: string;
+  aveStar: number;
 }
 
 const Author = ({ name, src, aveStar }: Props) => {
-  const shop = {
-    account: {
-      portrait: "b07ac6d8449d4f84abb1ba58d9371759",
-      total_avg_star: 4.893882,
-    },
-    name: "PIDO YOGA",
-    follower_count: 35344,
-    response_rate: 100,
-    response_time: 166,
-    last_active_time: 1602848596,
-  };
-
   return (
     <AuthorWrapper>
-      <AuthorImage
-        src={`https://cf.shopee.ph/file/${src ? src : shop.account.portrait}`}
-      />
+      <AuthorImage src={`https://cf.shopee.ph/file/${src}`} />
       <div style={{ fontSize: "14px" }}>
-        <span>{name ? name : shop.name}</span>
+        <span>{name}</span>
         <div>
-          {aveStar
-            ? aveStar.toFixed(1)
-            : shop.account.total_avg_star.toFixed(1)}{" "}
+          {aveStar.toFixed(1)}
           <Icon type='Star' size={12} />
         </div>
       </div>
--- frontend/src/components/Flex.tsx
+++ frontend/src/components/Flex.tsx
@@ -8,10 +8,16 @@ const Flex = styled.div<{
   scrollType?: "snap";
   align?: "center" | "flex-start" | "flex-end";
   border?: string;
+  borderBottom?: string;
   dir?: "column" | "row" | "column-reverse" | "row-reverse";
   gap?: number;
   grow?: number;
-  justify?: "center" | "flex-start" | "flex-end" | "space-between" | "around";
+  justify?:
+    | "center"
+    | "flex-start"
+    | "flex-end"
+    | "space-between"
+    | "space-around";
   margin?: string;
   overflow?: "auto" | "hidden" | "scroll";
   padding?: string;
@@ -24,6 +30,7 @@ const Flex = styled.div<{
   display: flex;
   scroll-snap-type: ${(props) => props.scrollType === "snap" && "x mandatory"};
   border: ${(props) => props.border && props.border};
+  border-bottom: ${(props) => props.borderBottom && props.borderBottom};
   width: ${(props) => props.width && props.width};
   height: ${(props) => props.height && props.height};
   background: ${(props) => props.background && props.background};
